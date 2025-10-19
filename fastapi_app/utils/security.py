5aa719793dda0c31cd31934298a3689ffbd0e934
kevinquintanah12 <kevinherreraq12@gmail.com>
2025-10-18 20:58:24 -0600

    Initial commit FastAPI Technicell

--- fastapi_app/utils/security.py
+++ fastapi_app/utils/security.py
@@ -1,16 +1,10 @@
-# utils/security.py
 from passlib.context import CryptContext
 
-# Configuramos bcrypt
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# 🔒 Cambiamos bcrypt por argon2
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def get_password_hash(password: str) -> str:
-    """Genera el hash de una contraseña, truncando si excede los 72 bytes"""
-    # Bcrypt solo permite hasta 72 bytes
-    if len(password.encode("utf-8")) > 72:
-        password = password[:72]
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    """Verifica que la contraseña coincida con su hash"""
     return pwd_context.verify(plain_password, hashed_password)
