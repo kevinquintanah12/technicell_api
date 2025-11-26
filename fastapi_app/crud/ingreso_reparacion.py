edec00b2260976b86b3f57f63f47bbb522d52779
kevinquintanah12 <kevinherreraq12@gmail.com>
2025-11-26 03:58:00 -0600

    kevo

--- fastapi_app/crud/ingreso_reparacion.py
+++ fastapi_app/crud/ingreso_reparacion.py
@@ -1,22 +1,29 @@
 from sqlalchemy.orm import Session
-from models.ingreso_reparacion import IngresoReparacion
-from schemas.ingreso_reparacion import IngresoReparacionCreate, IngresoReparacionUpdate
+from models.ingreso_reparacion import Ingreso
 
 
-def crear_ingreso(db: Session, data: IngresoReparacionCreate):
-    nuevo = IngresoReparacion(**data.dict())
+def crear_ingreso(db: Session, data: dict):
+    """
+    Crea un ingreso por reparación.
+    data debe ser un dict directo, no un Pydantic model.
+    """
+    nuevo = Ingreso(**data)
     db.add(nuevo)
     db.commit()
     db.refresh(nuevo)
     return nuevo
 
 
-def actualizar_ingreso(db: Session, ingreso_id: int, data: IngresoReparacionUpdate):
-    ingreso = db.query(IngresoReparacion).filter(IngresoReparacion.id == ingreso_id).first()
+def obtener_ingreso(db: Session, ingreso_id: int):
+    return db.query(Ingreso).filter(Ingreso.id == ingreso_id).first()
+
+
+def actualizar_ingreso(db: Session, ingreso_id: int, data: dict):
+    ingreso = obtener_ingreso(db, ingreso_id)
     if not ingreso:
         return None
 
-    for campo, valor in data.dict(exclude_unset=True).items():
+    for campo, valor in data.items():
         setattr(ingreso, campo, valor)
 
     db.commit()
