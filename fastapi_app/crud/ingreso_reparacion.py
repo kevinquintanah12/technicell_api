fb00e77d48e34cf14879fccec497d68961f17c61
kevinquintanah12 <kevinherreraq12@gmail.com>
2025-11-26 21:17:14 -0600

    kevo

--- fastapi_app/crud/ingreso_reparacion.py
+++ fastapi_app/crud/ingreso_reparacion.py
@@ -3,11 +3,7 @@ from models.ingreso_reparacion import IngresoReparacion
 
 
 def crear_ingreso(db: Session, data: dict):
-    """
-    Crea un ingreso por reparación.
-    data debe ser un dict directo, no un Pydantic model.
-    """
-    nuevo = Ingreso(**data)
+    nuevo = IngresoReparacion(**data)
     db.add(nuevo)
     db.commit()
     db.refresh(nuevo)
@@ -15,7 +11,7 @@ def crear_ingreso(db: Session, data: dict):
 
 
 def obtener_ingreso(db: Session, ingreso_id: int):
-    return db.query(Ingreso).filter(Ingreso.id == ingreso_id).first()
+    return db.query(IngresoReparacion).filter(IngresoReparacion.id == ingreso_id).first()
 
 
 def actualizar_ingreso(db: Session, ingreso_id: int, data: dict):
